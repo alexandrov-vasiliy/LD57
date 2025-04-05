f67b5805749dfb49297328a22a8bc0433e78c0d1
Wanabii <aleksandrovtimothy@gmail.com>
2025-04-05 16:14:51 +0200

    added movement system.

---
+++ Assets/#Game/Sonar/Scripts/SonarMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class SonarMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb;
+    private Vector3 moveInput;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector3(moveInput.x, moveInput.y, rb.linearVelocity.z) * moveSpeed;
+    }
+}
