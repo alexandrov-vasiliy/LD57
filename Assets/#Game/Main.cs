b97d094c630d9dcacbf9ea51181bbbab10151f31
alexandrov-vasiliy <zowel.vasili@yandex.ru>
2025-04-06 22:13:45 +0300

    added post proccesing

--- Assets/#Game/Main.cs
+++ Assets/#Game/Main.cs
@@ -8,17 +8,14 @@ using UnityEngine;
 public class Main : MonoBehaviour
 {
     [SerializeField] private MarkerComponent _marker;
-    [SerializeField] private MonitorOffEffectController _dataMonitor;
     [SerializeField] private CRTGlitchEffect _dataMonitorGlitch;
 
-    [SerializeField] private MonitorOffEffectController _sonarMonitor;
     [SerializeField] private CRTGlitchEffect _sonarMonitorGlitch;
     [SerializeField] private GameObject _Game;
     void Awake()
     {
         G.MarkerPool = new ObjectPool<MarkerComponent>(_marker, 200, transform);
-        _dataMonitor.OnMonitor();
-        _sonarMonitor.OnMonitor();
+
     }
 
     private void OnEnable()
